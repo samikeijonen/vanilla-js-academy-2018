f9bd3eb1eac44cb48cd7b24982a1d2fa7d84cd44
Sami Keijonen <sami.keijonen@foxnet.fi>
2019-01-03 10:53:08 +0200

    Add articles cached task.

---
+++ project-10/app.js
@@ -0,0 +1,219 @@
+var cachedArticles = (function () {
+    'use strict;'
+
+	// Public APIs
+	var publicAPIs = {};
+	var settings;
+
+	/**
+	 * Dynamically vary the API endpoint
+	 * @return {String} The API endpoint
+	 */
+	var getEndpoint = function () {
+		var endpoint = 'https://vanillajsacademy.com/api/';
+		var random = Math.random();
+		if (random < 0.3) return endpoint + 'pirates.json';
+		if (random < 0.6) return endpoint + 'pirates2.json';
+		return endpoint + 'fail.json';
+	};
+
+	// Defaults
+	var defaults = {
+		selectorApp: '#app',
+	};
+
+	/**
+	 * Merge two or more objects together.
+	 * @param   {Object}   objects  The objects to merge together
+	 * @returns {Object}            Merged values of defaults and options
+	*/
+	var extend = function () {
+		// Variables
+		var extended = {};
+
+		// Merge the object into the extended object
+		var merge = function ( obj ) {
+			for ( var prop in obj ) {
+				if ( obj.hasOwnProperty( prop ) ) {
+					extended[prop] = obj[prop];
+				}
+			}
+		};
+
+		// Loop through each object and conduct a merge
+		for ( var i = 0; i < arguments.length; i++ ) {
+			merge(arguments[i]);
+		}
+
+		return extended;
+    };
+
+	/**
+	 * Render failure message.
+	 *
+	 * @param {Object} app
+	 * @param {Object} data
+	 */
+	var renderFailure = function( app, data ) {
+		// Get data from localStorage.
+		var savedArticles = JSON.parse( localStorage.getItem( 'savedArticles' ) );
+
+		// Check if there is data.
+		if ( savedArticles ) {
+			// There is already data, use it to render articles markup.
+			renderArticles( app, savedArticles.data.articles );
+		} else {
+			app.textContent = 'Something went wrong. Try again?';
+		}
+	};
+
+	/**
+	 * Make XHR request to API.
+	 *
+	 * @param {String}   url     The URL to make the request to
+	 * @param {String}   method  API method (GET, POST)
+	 * @param {Function} success Callback when successful
+	 * @param {Function} error   Callback when error
+	 */
+	var makeRequest = function ( url, method, success, error ) {
+		// Make sure a URL and method were provided
+		if ( ! url || ! method ) {
+			return;
+		}
+
+		// Set up our HTTP request
+		var xhr = new XMLHttpRequest();
+
+		// Setup our listener to process request state changes
+		xhr.onreadystatechange = function () {
+			// Only run if the request is complete
+			if ( xhr.readyState !== xhr.DONE ) {
+				return;
+			}
+
+			// Process our return data
+			if ( xhr.status >= 200 && xhr.status < 300 ) {
+				// Run the success callback.
+				if ( success && typeof success === 'function' ) {
+					success( xhr );
+				}
+			} else {
+				// Run the error callback.
+				if ( error && typeof error === 'function' ) {
+					error( xhr );
+				}
+			}
+		};
+
+		// Create and send a request
+		// Defaults to GET
+		xhr.open( method, url );
+		xhr.send();
+	};
+
+	var isDataValid = function ( saved ) {
+
+		// Check that there's data, and a timestamp key.
+		if (!saved || !saved.data || !saved.timestamp) {
+			return false;
+		}
+
+		// Get the difference between the timestamp and current time.
+		var difference = new Date().getTime() - saved.timestamp;
+
+		// Convert the difference into hours.
+		var oneHour = 1000 * 60 * 60;
+		var convertedTime = difference / oneHour;
+
+		// Check if it's been less than an hour.
+		if ( convertedTime < 1 ) {
+			return true;
+		}
+
+	};
+
+	/**
+	 * Sanitize and encode all HTML in a user-submitted string
+	 * @param  {String} str The user-submitted string
+	 * @return {String} str The sanitized string
+	 */
+	var sanitizeHTML = function ( str ) {
+		var temp = document.createElement( 'div' );
+		temp.textContent = str;
+		return temp.innerHTML;
+	};
+
+	var renderArticles = function ( app, articles ) {
+		articles.forEach( function ( article ) {
+			app.innerHTML +=
+			'<article class="entry">' +
+				'<p class="entry__byline">Written by '  + sanitizeHTML( article.author )  + '. Published ' + sanitizeHTML( article.pubdate ) + '</p>'  +
+				'<h2 class="entry__title">'  + sanitizeHTML( article.title ) + '</h2>' +
+				'<p class="entry__summary">' + sanitizeHTML( article.article ) + '</p>'  +
+			'</article>';
+		} );
+	};
+
+	/**
+	 * Public API.
+	 */
+	publicAPIs.init = function ( options ) {
+
+		// Merge options into defaults.
+		settings = extend( defaults, options || {} );
+
+		// App element.
+		var app = document.querySelector( settings.selectorApp );
+
+		if ( ! app ) {
+			return;
+		}
+
+		// Get data from localStorage.
+		var savedArticles = JSON.parse( localStorage.getItem( 'savedArticles' ) );
+
+		// Check its validity.
+		if ( isDataValid( savedArticles ) ) {
+			// The data is still good, use it to render articles markup.
+			renderArticles( app, savedArticles.data.articles );
+		} else {
+			// Get fresh data and use that instead.
+
+			// Get URL.
+			var url = getEndpoint();
+
+			// Make request to articles API.
+			makeRequest( url, 'GET',
+				// Success function.
+				function ( data ) {
+
+					data = JSON.parse( data.responseText );
+					console.log( data );
+					console.log( data.articles );
+					var articles = data.articles;
+
+					// Setup the localStorage data.
+					var saved = {
+						data: data,
+						timestamp: new Date().getTime()
+					};
+
+					// Save articles to localStorage.
+					localStorage.setItem( 'savedArticles', JSON.stringify( saved ) );
+
+					// Render articles markup.
+					renderArticles( app, articles );
+				},
+				// Failure function.
+				function ( data ) {
+					renderFailure( app, data );
+				}
+			);
+		}
+    };
+
+    return publicAPIs;
+})();
+
+// Init the App.
+cachedArticles.init();
